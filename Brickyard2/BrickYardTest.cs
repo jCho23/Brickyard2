a0f8c891c82d6adccce710d33a9597410946bf41
JuneCho23 <junecho.ucb@gmail.com>
2017-03-04 18:32:35 -0800

    Test Automation Scripts for BrickYard

---
+++ Brickyard2/BrickYardTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace BrickYardTC
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			// TODO: If the Android app being tested is included in the solution then open
+			// the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			app = ConfigureApp
+				.Android
+				// TODO: Update this path to point to your Android app and uncomment the
+				// code if the app is not included in the solution.
+				.ApkFile("BrickYard.apk")
+				.StartApp();
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			app.WaitForElement(x => x.Id("race_cell"));
+			app.Flash(x => x.Id("race_cell").Index(0));
+			app.Tap(x => x.Id("race_cell").Index(0));
+
+			app.WaitForElement(june => june.Id("tab_title").Text("TICKETS"));
+			app.Tap(june => june.Id("tab_title").Text("TICKETS"));
+		}
+	}
+}
